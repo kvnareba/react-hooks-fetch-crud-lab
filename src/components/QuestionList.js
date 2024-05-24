ed6f63d07291d704b8d87e55e68a2e97bc199c64
kvn <kvnareba@gmail.com>
2024-05-24 07:46:06 +0300

    com

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -10,3 +10,35 @@ function QuestionList() {
 }
 
 export default QuestionList;
+})
+}
+
+
+function handleAnswerChange(id, correctIndex) {
+  fetch(`http://localhost:4000/questions/${id}`, {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ correctIndex }),
+  })
+    .then((r) => r.json())
+    .then((updatedQuestion) => {
+      const updatedQuestions = quiz.map((quiz) => {
+        if (quiz.id === updatedQuestion.id) return updatedQuestion;
+        return quiz;
+      });
+      setQuiz(updatedQuestions);
+    });
+}
+  return (
+    <section>
+      <h1>Quiz Questions</h1>
+      <ul>
+      {quiz.map((quiz) => (
+        <QuestionItem key={quiz.id} question={quiz} onDeleteClick={handleClickDelete} onAnswerchange={handleAnswerChange}/>
+      ))}
+    </ul>
+    </section>
+  );
+}
